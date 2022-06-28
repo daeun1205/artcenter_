25140cf0bcb7f04c32436a1affb886298d543fa7
daeun1205 <102287829+daeun1205@users.noreply.github.com>
2022-06-29 01:39:31 +0900

    Update artcenter.js

--- js/artcenter.js
+++ js/artcenter.js
@@ -50,5 +50,21 @@ $(document).ready(function(){
         $('.image img').attr('src', dataImage)
         
       });
+
+      // 상단이동버튼
+      var but=$('.top_but');
+      $(window).scroll(function(){
+          if($(window).scrollTop() > 300){
+              but.fadeIn();
+          }else{
+              but.fadeOut();
+          }
+      });
+      but.on('click',function(e){
+          e.preventDefault();
+          $('html,body').animate({
+              scrollTop:0
+          },300);
+      });
       
 });//end
